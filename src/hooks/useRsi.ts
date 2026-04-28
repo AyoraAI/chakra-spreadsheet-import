f99586414345993a1a5bbcb4d3199dbae1e74b6e
Gordon McKenzie <gordon@ayora.ai>
2026-04-28 11:37:33 +0100

    fix: build failure

--- src/hooks/useRsi.ts
+++ src/hooks/useRsi.ts
@@ -2,13 +2,25 @@ import { useContext } from "react"
 import { RsiContext } from "../components/Providers"
 import type { RsiProps } from "../types"
 import type { MarkRequired } from "ts-essentials"
-import type { defaultRSIProps } from "../"
 import type { Translations } from "../translationsRSIProps"
 
-export const useRsi = <T extends string>() =>
-  useContext<
-    MarkRequired<RsiProps<T>, keyof typeof defaultRSIProps> & {
-      translations: Translations
-    }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  >(RsiContext)
+type DefaultRsiPropKeys =
+  | "autoMapHeaders"
+  | "autoMapSelectValues"
+  | "allowInvalidSubmit"
+  | "autoMapDistance"
+  | "isNavigationEnabled"
+  | "translations"
+  | "uploadStepHook"
+  | "selectHeaderStepHook"
+  | "matchColumnsStepHook"
+  | "dateFormat"
+  | "parseRaw"
+
+type UseRsiValue<T extends string> = MarkRequired<RsiProps<T>, DefaultRsiPropKeys> & {
+  translations: Translations
+}
+
+export const useRsi = <T extends string>(): UseRsiValue<T> =>
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  useContext<UseRsiValue<T>>(RsiContext)
